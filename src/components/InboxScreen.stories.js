1792e0ea26f38d42d78ccf443e005e78b882c312
Quynh <q.lenhuquynh@gmail.com>
2019-03-18 16:09:27 +0200

    add knobs

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Provider } from "react-redux";
+
 import { InboxScreen } from "./InboxScreen";
-import App from "../App";
+import { defaultTasks } from "./TaskList.stories";
+
+// A super-simple mock of a redux store
+const store = {
+  getState: () => {
+    return {
+      tasks: defaultTasks
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action("dispatch")
+};
 
 storiesOf("InboxScreen", module)
-  .addDecorator(story => <App>{story()}</App>)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
   .add("default", () => <InboxScreen />)
   .add("error", () => <InboxScreen error="Something" />);
---
+++ storybook-static/precache-manifest.84a0120ab658f3ce539eb9501d51ae9f.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.8e9d3aa3f00a76465672.bundle.js"
+  },
+  {
+    "url": "runtime~main.8e9d3aa3f00a76465672.bundle.js"
+  },
+  {
+    "revision": "78cefc9c0b05a264993f",
+    "url": "main.css"
+  },
+  {
+    "url": "main.8e9d3aa3f00a76465672.bundle.js"
+  },
+  {
+    "revision": "4f4c490cf263fee5275240b7e96b3f4f",
+    "url": "iframe.html"
+  }
+];
